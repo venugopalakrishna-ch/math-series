e8f9900057984b9f20f3e2c6cab10776c3203f70
venugopalakrishna-ch <venugopalakrishna.ch@gmail.com>
2020-04-22 21:00:20 +0530

    fibonacci

--- tests/test_math_series.py
+++ tests/test_math_series.py
@@ -1,5 +1,44 @@
 from math_series import __version__
-
+from math_series.series import fibonacci
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+def test_fib_zero():
+    actual = fibonacci(0)
+    expected = 0
+    assert actual == expected
+
+def test_fib_one():
+    actual = fibonacci(1)
+    expected = 1
+    assert actual == expected
+
+def test_fib_two():
+    actual = fibonacci(2)
+    expected = 1
+    assert actual == expected
+
+def test_fib_theree():
+    actual = fibonacci(3)
+    expected = 2
+    assert actual == expected
+
+def test_fib_four():
+    actual = fibonacci(4)
+    expected = 3
+    assert actual == expected
+
+def test_fib_five():
+    actual = fibonacci(5)
+    expected = 5
+    assert actual == expected
+
+def test_fib_100():
+    actual = fibonacci(100)
+    expected = 190
+    assert actual == expected
+
+
+
