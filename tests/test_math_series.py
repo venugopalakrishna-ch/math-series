b2ec05021eae128811a5958b10e09574902d6730
venugopalakrishna-ch <venugopalakrishna.ch@gmail.com>
2020-04-24 19:33:09 +0530

    fibonacci using iterative method

--- tests/test_math_series.py
+++ tests/test_math_series.py
@@ -1,5 +1,6 @@
 from math_series import __version__
 from math_series.series import fibonacci
+from math_series.series import fibonacci_iteration
 
 def test_version():
     assert __version__ == '0.1.0'
@@ -39,6 +40,49 @@ def test_fib_six():
     actual = fibonacci(6)
     expected = 8
     assert actual == expected
+
+
+def test_fib_iteration_zero():
+    actual = fibonacci_iteration(0)
+    expected = 0
+    assert actual == expected
+
+def test_fib_iteration_one():
+    actual = fibonacci_iteration(1)
+    expected = 1
+    assert actual == expected
+
+def test_fib_iteration_two():
+    actual = fibonacci_iteration(2)
+    expected = 1
+    assert actual == expected
+
+def test_fib_iteration_three():
+    actual = fibonacci_iteration(3)
+    expected = 2
+    assert actual == expected
+
+def test_fib_iteration_four():
+    actual = fibonacci_iteration(4)
+    expected = 3
+    assert actual == expected
+
+def test_fib_iteration_five():
+    actual = fibonacci_iteration(5)
+    expected = 5
+    assert actual == expected
+
+def test_fib_iteration_six():
+    actual = fibonacci_iteration(6)
+    expected = 8
+    assert actual == expected
+
+def test_fib_iteration_seven():
+    actual = fibonacci_iteration(7)
+    expected = 13
+    assert actual == expected
+
+
 
 
 
