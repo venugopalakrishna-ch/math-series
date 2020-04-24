ef5f839a9c52f5728b3b77004b0f6eb5afeb1e0c
venugopalakrishna-ch <venugopalakrishna.ch@gmail.com>
2020-04-24 20:06:32 +0530

    Lucas Series

--- tests/test_math_series.py
+++ tests/test_math_series.py
@@ -1,6 +1,8 @@
 from math_series import __version__
 from math_series.series import fibonacci
 from math_series.series import fibonacci_iteration
+from math_series.series import lucas_recursive
+from math_series.series import lucas_iterative
 
 def test_version():
     assert __version__ == '0.1.0'
@@ -82,9 +84,44 @@ def test_fib_iteration_seven():
     expected = 13
     assert actual == expected
 
+def test_lucas_recursiv_zero():
+    assert lucas_recursive(0) == 0
 
+def test_lucas_recursiv_one():
+    assert lucas_recursive(1) == 2
 
+def test_lucas_recursiv_two():
+    assert lucas_recursive(2) == 1
 
+def test_lucas_recursiv_three():
+    assert lucas_recursive(3) == 3
 
+def test_lucas_recursiv_four():
+    assert lucas_recursive(4) == 4
 
+def test_lucas_recursiv_five():
+    assert lucas_recursive(5) == 7
 
+def test_lucas_recursiv_six():
+    assert lucas_recursive(6) == 11
+
+def test_lucas_iterative_zero():
+    assert lucas_iterative(0) == 0
+
+def test_lucas_iterative_one():
+    assert lucas_iterative(1) == 2
+
+def test_lucas_iterative_two():
+    assert lucas_iterative(2) == 1
+
+def test_lucas_iterative_three():
+    assert lucas_iterative(3) == 3
+
+def test_lucas_iterative_four():
+    assert lucas_iterative(4) == 4
+
+def test_lucas_iterative_five():
+    assert lucas_iterative(5) == 7
+
+def test_lucas_iterative_six():
+    assert lucas_iterative(6) == 11
